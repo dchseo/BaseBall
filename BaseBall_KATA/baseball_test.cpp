511d4b6aa518fc73646dd7082c9a5fd28be1adfb
Duckchan-Seo <duckchan.seo@samsung.com>
2026-07-01 13:40:25 +0900

    [feature] page 22

--- BaseBall_KATA/baseball_test.cpp
+++ BaseBall_KATA/baseball_test.cpp
@@ -19,6 +19,13 @@ TEST(BaseballGame, ThrowExceptionWhenInputLengthUsUnmached)
 
 }
 
+TEST(BaseballGame, ThrowExceptionWhenInvalidChar)
+{
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12s")), invalid_argument);
+
+}
+
 int main() {
 	testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
