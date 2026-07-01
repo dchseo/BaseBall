09720326b63eb174fd7129bade000056fd200281
Duckchan-Seo <duckchan.seo@samsung.com>
2026-07-01 13:11:36 +0900

    [feature] page 19

--- BaseBall_KATA/baseball.cpp
+++ BaseBall_KATA/baseball.cpp
@@ -1,6 +1,16 @@
+#include <stdexcept>
+
+using namespace std;
+
+
 
 class Baseball {
 
 public:
+
+	void guess(const string& string)
+	{
+		throw length_error("must be three letters.");
+	}
 
 };
--- BaseBall_KATA/baseball_test.cpp
+++ BaseBall_KATA/baseball_test.cpp
@@ -1,10 +1,22 @@
 #include "gmock/gmock.h"
 #include "baseball.cpp"
+#include "string"
+#include "baseball_test.h"
 
+using std::string;
+
+using namespace testing;
 
 TEST(BaseballGame, TryGameTest)
 {
 	EXPECT_EQ(1, 1);
+}
+
+TEST(BaseballGame, ThrowExceptionWhenInputLengthUsUnmached)
+{
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12")), length_error);
+
 }
 
 int main() {
